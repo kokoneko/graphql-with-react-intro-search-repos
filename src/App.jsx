039f8232e5aa9972a29b024128341f3baccc0cbe
cocowan <maegawaomaru@gmail.com>
2021-03-21 15:39:26 +0900

    lint

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import { ApolloProvider } from "react-apollo"
 import gql from "graphql-tag"
 import { Query } from "react-apollo"
